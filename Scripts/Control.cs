f8dab55fbaf34dc8a8f33a620b25b8b314c84dcf
Mason <yonngwang@outlook.com>
2019-09-09 01:20:20 -0400

    9.9 Assignment1

---
+++ Scripts/Control.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform Turret;
+    public GameObject bulletObj;
+    public GameObject output;
+    public Transform bulletParent;
+
+    public float KgofPowderper = 1000f;   //N
+    public float MassofBall = 1000f;    //kg
+    static float stepsize = 0f;  //step size
+
+
+    GameObject newBullet;
+    private void Awake()
+    {
+        stepsize = Time.fixedDeltaTime * 1f;
+
+    }
+
+    void Start()
+    {
+        
+
+    }
+
+    private void FixedUpdate()
+    {
+        RotatingControl();
+    }
+
+
+    //Turret Rotating
+    void RotatingControl()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+
+            Turret.localRotation = Quaternion.Euler(Turret.localRotation.eulerAngles.x+2f, Turret.localRotation.eulerAngles.y, Turret.localRotation.eulerAngles.z);
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            Turret.localRotation = Quaternion.Euler(Turret.localRotation.eulerAngles.x - 2f, Turret.localRotation.eulerAngles.y, Turret.localRotation.eulerAngles.z);
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            Turret.localRotation = Quaternion.Euler(Turret.localRotation.eulerAngles.x, Turret.localRotation.eulerAngles.y, Turret.localRotation.eulerAngles.z + 2f);
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            Turret.localRotation = Quaternion.Euler(Turret.localRotation.eulerAngles.x, Turret.localRotation.eulerAngles.y, Turret.localRotation.eulerAngles.z - 2f);
+        }
+        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 tmp = new Vector3(output.transform.position.x, output.transform.position.y, output.transform.position.z);
+
+            GameObject newBullet = Instantiate(bulletObj, tmp, transform.rotation) as GameObject;
+
+            //Parent it to get a less messy workspace
+            newBullet.transform.parent = bulletParent;
+            Vector3 newPosition = Vector3.zero;
+            Vector3 newVelocity = Vector3.zero;
+
+            //may probably use the startcoroutine 
+
+            //Add velocity to the bullet with a rigidbody, Unity internal physics engine
+            //newBullet.GetComponent<Rigidbody>().velocity = KgofPowderper / MassofBall * output.transform.up;
+            newBullet.GetComponent<Rigidbody>().isKinematic = true;
+            Vector3 acceleratingfactor = Physics.gravity;
+            StartCoroutine(IntegrationMethods_Prepare(newBullet, tmp, KgofPowderper / MassofBall * output.transform.up, newPosition, newVelocity, acceleratingfactor));
+        }
+    }
+
+    //Parameters Preparation and then the waiting for a while the ball will be delete
+    IEnumerator IntegrationMethods_Prepare(GameObject Bullet, Vector3 currentPosition, Vector3 currentVelocity, Vector3 newPosition, Vector3 newVelocity, Vector3 acceleratingFactor)
+    {
+        Vector3 BulletPosition = Bullet.transform.position;     
+        
+
+        while(true)  //still in the flying
+        {
+            if (Bullet.transform.position.y <= 0.3f)
+            {
+
+                Bullet.GetComponent<Rigidbody>().isKinematic = false;
+                break;
+            }
+            yield return new WaitForEndOfFrame();
+            //out meanse it is the output value and muse be initilized or updated.
+            IntegrationMethods.CurrentIntegrationMethod(stepsize, currentPosition, currentVelocity, out newPosition, out newVelocity,ref acceleratingFactor);
+            currentPosition = newPosition;
+            currentVelocity = newVelocity;
+            Bullet.GetComponent<Rigidbody>().MovePosition(currentPosition);
+            //Bullet.transform.position = currentPosition;
+            BulletPosition = currentPosition;
+            
+        }
+
+        yield return new WaitForSeconds(0.8f);
+        Destroy(Bullet);
+
+    }
+
+    // wait for a while, and then disapper
+}
---
+++ Scripts/IntegrationMethods.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntegrationMethods
+{
+    // Start is called before the first frame update
+
+    public static void CurrentIntegrationMethod(float h,
+    Vector3 currentPosition,
+    Vector3 currentVelocity,
+    out Vector3 newPosition,
+    out Vector3 newVelocity,
+    ref Vector3 acceleratingfactor)
+    {
+        //MonoBehaviour.print("here");
+        //BackwardEuler(h, currentPosition, currentVelocity, out newPosition, out newVelocity,ref acceleratingfactor);
+        //MidPointMethod(h, currentPosition, currentVelocity, out newPosition, out newVelocity, ref acceleratingfactor);
+        ForthOrderRungeKuttaMethod(h, currentPosition, currentVelocity, out newPosition, out newVelocity, ref acceleratingfactor);
+
+    }
+
+    public static void BackwardEuler(float h,
+        Vector3 currentPosition,
+        Vector3 currentVelocity,
+        out Vector3 newPosition,
+        out Vector3 newVelocity,
+        ref Vector3 acceleratingfactor)
+    {
+        Vector3 acceleratingFactor = acceleratingfactor;
+
+        //out parameters must be assigned value in the called method
+        //BackwardEuler
+        newVelocity = currentVelocity + h * acceleratingFactor;
+        newPosition = currentPosition + h * newVelocity;
+
+    }
+
+    // o(t^3)
+    public static void MidPointMethod(float h,
+        Vector3 currentPosition,
+        Vector3 currentVelocity,
+        out Vector3 newPosition,
+        out Vector3 newVelocity,
+        ref Vector3 acceleratingfactor)
+    {
+        Vector3 acceleratingFactor = acceleratingfactor;
+
+        Vector3 HalfnewVelocity = currentVelocity + acceleratingfactor*h/2.0f;
+        Vector3 HalfnewPosition = currentPosition + HalfnewVelocity*h/2.0f;
+        newVelocity = currentVelocity + h * acceleratingFactor;
+        newPosition = currentPosition + h * HalfnewVelocity;
+    }
+
+    public static void ForthOrderRungeKuttaMethod(float h,
+        Vector3 currentPosition,
+        Vector3 currentVelocity,
+        out Vector3 newPosition,
+        out Vector3 newVelocity,
+        ref Vector3 acceleratingfactor)
+    {
+        Vector3[] position = new Vector3[5];
+        Vector3[] velocity = new Vector3[5];
+
+
+        ///need to improve
+        position[0] = currentPosition;
+        velocity[0] = currentVelocity;
+
+        position[1] = position[0];
+        velocity[1] = velocity[0];
+
+        position[2] = position[0] + h / 2.0f * velocity[1];
+        velocity[2] = velocity[0] + h / 2.0f * acceleratingfactor;
+
+        position[3] = position[0] + h / 2.0f * velocity[2];
+        velocity[3] = velocity[0] + h / 2.0f * acceleratingfactor;
+
+        position[4] = position[0] + h * velocity[3];
+        velocity[4] = velocity[0] + h * acceleratingfactor;
+
+        //combined derivates
+        newPosition = position[0] + h / 6.0f * (velocity[1] + 2*velocity[2] + 2*velocity[3] + velocity[4]);
+        newVelocity = velocity[0] + h * acceleratingfactor;   //const a
+
+    }
+
+}
